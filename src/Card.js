ec06e76db469309f5b5f030e6d4faf94b1adeeea
Jared Gonzales <jrg2221@columbia.edu>
2022-04-25 19:30:01 -0400

    Minor bug fixes

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Card(props) {
     return (
         <div class="card">
-            <img src="https://www.w3schools.com/howto/img_avatar.png" />
+            <img href="https://www.w3schools.com/howto/img_avatar.png" alt="placeholder" />
             <div class="card-container">
                 <h3>{props.name}</h3>
                 <p>{props.year}</p>
@@ -16,9 +16,17 @@ export default function Card(props) {
 }
 
 function buildDatabase() {
+    var container = Document.GetElementByID("database-container");
+    fetch("/getAlumni").then(response) => {
+        for (var i = 0; i < response.count; i++) {
+            current_alumni = response[i]
+            var card = <Card name=current_alumni.name year=current_alumni.year />
+            container.addChild(card)
+        }
+    }
     return (
         <div class="database-container" id="database-container">
-            <h1>Loading...</h1>
+            <h1 id="database-loading">Loading...</h1>
         </div>
         // Fetch Firestore documents
         // Get count
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import { Link } from "react-router-dom";
 
 export function NavBar(props) {
     return (
